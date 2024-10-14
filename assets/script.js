1268d0b80167a4a8bde548006bb83362d33f963e
ManuelTrappolini <manuel.trappolini@gmail.com>
2024-10-14 14:55:41 +0200

    add markup into DOM

--- assets/script.js
+++ assets/script.js
@@ -11,37 +11,37 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "./assets/img/male1.png"
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "./assets/img/female1.png"
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "./assets/img/male2.png"
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "./assets/img/female2.png"
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "./assets/img/male3.png"
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
+    img: "./assets/img/female3.png"
   }
 ];
 
@@ -49,7 +49,26 @@ const cardEl = document.getElementById('card')
 const teamEl = document.querySelector('#team')
 
 
+
+
+
+let finalMarkup = ''
+
+for (let i = 0; i < teamMembers.length; i++) {
+  const teamMember = teamMembers[i];
+  console.log(teamMember);
+  const {name, role , email, img} = teamMember
+  const markup = generateTeamMemberCard(teamMember)
+  finalMarkup += markup
+  console.log(markup);
+  
+}
+
+teamEl.innerHTML = finalMarkup
+
+
 function generateTeamMemberCard(teamMember){
+  const {name, role , email, img} = teamMember
   return `
     <div class="col-4">
       <div class="card">
@@ -59,19 +78,7 @@ function generateTeamMemberCard(teamMember){
         <h4>e${email}</h4>
       </div>
     </div>  
-  `}
-
-
-
-
-for (let i = 0; i < teamMembers.length; i++) {
-  const teamMember = teamMembers[i];
-  console.log(teamMember);
-  const {name, role , email, img} = teamMember
-  const markup = generateTeamMemberCard(teamMember)
-  teamEl.innerHTML = markup
+    `
+  }
   
-}
-
-
-
+  document.querySelector('img').style.width = 100%
